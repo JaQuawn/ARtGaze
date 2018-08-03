3ad234bbf71598d492cc56a83f7f55d4fd976c89
jaquawn <jd23@hawaii.edu>
2018-08-03 10:18:52 -1000

    Initial Commit

---
+++ ARt Gaze/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARt Gaze
+//
+//  Created by Jaquawn Yarborough on 8/3/18.
+//  Copyright © 2018 Jaquawn Yarborough. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
